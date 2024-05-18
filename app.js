89e43a88dcb8f72822c2da04d6f6a26e65a5693c
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-05-18 21:29:27 +0600

    again update the app.js file

--- app.js
+++ app.js
@@ -13,28 +13,28 @@ const addTask = () => {
 };
 
 
-updateTasksList = () => {
-    const taskList = document.getElementById('task-list')
-    taskList.innerHTML = ''
+const updateTasksList = () => {
+    const taskList = document.getElementById("task-list")
+    taskList.innerHTML = "";
 
 
     tasks.forEach((task, index) => {
-        const listItem = document.createElement('li')
+        const listItem = document.createElement("li")
 
         listItem.innerHTML = `
             <div class="taskItem">
-                <div class="task ${task.completed ? 'completed':''}">
+                <div class="task ${task.completed ? "completed":""}">
                     <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""}/>
                     <p>${task.text}</p>
                 </div>
-                <div class="icon">
-                    <img src="./images/edit.png" onClick=editTask(${index})>
-                    <img src="./images/delet.png" onClick=deletTask(${index})>
+                <div class="icons">
+                    <img src="./images/edit.png" onClick = "editTask(${index})">
+                    <img src="./images/delet.png" onClick = "deletTask(${index})">
                 </div>
             </div>
             `;
 
-        listItem.addEventListener('change', () => toggleTaskComplete(index));
+        listItem.addEventListener("change", () => toggleTaskComplete(index));
         taskList.appendChild(listItem);
     });
 };
